139358e6d379658beb16026183c91f02c9118fb8
Elif Balci <mrselifbalci@gmail.com>
2023-02-03 17:30:19 +0000

    first commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -9,13 +9,17 @@ import {getStorage} from 'firebase/storage'
 
 // Your web app's Firebase configuration  
 const firebaseConfig = {
-    apiKey: "AIzaSyDmsh1qd1p7SmSzip2vObhzKDtiinQFAH8",
-    authDomain: "cohort-13.firebaseapp.com",
-    projectId: "cohort-13",
-    storageBucket: "cohort-13.appspot.com",
-    messagingSenderId: "440323145771",
-    appId: "1:440323145771:web:e7bdffbfb8adf0266d6a6e"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   }; 
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);  
